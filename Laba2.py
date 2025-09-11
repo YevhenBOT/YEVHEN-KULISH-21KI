72a72796da5856e2a7277c185660f649af957799
Yevhen <evgenkulis584@gmail.com>
2025-09-11 13:54:18 +0300

    Laba2.py

--- Laba2.py
+++ Laba2.py
@@ -1,9 +1,21 @@
-data = [1, 3, 2, 17, 20, 6, 7, 22, 99, 10, 'c', 'b', 'a', 'd', 'e', 'g', 'f', 'z', 'h', 'i']
-ints = sorted(x for x in data if type(x) is int)
-strs = sorted(x for x in data if type(x) is str)
-list = ints + strs
-even_numbers = [x for x in ints if x % 2 == 0]
-caps = [x.upper() for x in strs]
-print('Відсортований список : ',list)
-print('Парні числа: ', even_numbers)
-print('Капсом: ', caps)
+list = [5, 8, 'apple', 2, 'dog', 11, 'banana', 4, 'zoo', 10, 1, 'cat', 6, 'orange', 3, 'grape', 12, 'kiwi', 9, 'mango']
+
+ints = []
+strs = []
+
+for x in list:
+    if isinstance(x, int):
+        ints.append(x)
+    elif isinstance(x, str):
+        strs.append(x)
+
+ints.sort()
+strs.sort()
+
+main_sorted = ints + strs
+even_ints = [x for x in ints if x % 2 == 0]
+strs_caps = [s.upper() for s in strs]
+
+print("Відсортований список:", main_sorted)
+print("Список кратних 2:", even_ints)
+print("Список рядків у капсі:", strs_caps)
